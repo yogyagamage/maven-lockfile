ebd60c7c9722d6baf5e171b77ec306a11d81fee9
yogyagamage <yogyagamage@gmail.com>
2024-05-05 10:53:58 +0200

    Add new warning for package tamperings

--- maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/ValidateChecksumMojo.java
+++ maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/ValidateChecksumMojo.java
@@ -51,30 +51,32 @@ public class ValidateChecksumMojo extends AbstractLockfileMojo {
             }
             if (!lockFileFromFile.equals(lockFileFromProject)) {
                 var diff = LockFileDifference.diff(lockFileFromFile, lockFileFromProject);
-                StringBuilder sb = new StringBuilder();
-                sb.append("Lock file validation failed. Differences:");
-                sb.append("\n");
-                sb.append("Your lockfile from file is for:"
+                String sa = "Lock file validation failed. There are packages which are modified." + "\n"
+                        + "Modified dependencies in project: " + "\n"
+                        + JsonUtils.toJson(diff.getDependenciesWithInvalidChecksums())
+                        + "Modified plugins in project: " + "\n"
+                        + JsonUtils.toJson(diff.getPluginsWithInvalidChecksums())
+                        + "\n";
+                String sb = "Lock file validation failed. Differences:" + "\n"
+                        + "Your lockfile from file is for:"
                         + lockFileFromFile.getGroupId().getValue()
                         + ":" + lockFileFromFile.getName().getValue() + ":"
-                        + lockFileFromFile.getVersion().getValue() + "\n");
-                sb.append("Your generated lockfile is for:"
+                        + lockFileFromFile.getVersion().getValue() + "\n" + "Your generated lockfile is for:"
                         + lockFileFromProject.getGroupId().getValue() + ":"
                         + lockFileFromProject.getName().getValue() + ":"
-                        + lockFileFromProject.getVersion().getValue() + "\n");
-                sb.append("Missing dependencies in lock file:\n ");
-                sb.append(JsonUtils.toJson(diff.getMissingDependenciesInFile()));
-                sb.append("\n");
-                sb.append("Missing dependencies in project:\n ");
-                sb.append(JsonUtils.toJson(diff.getMissingDependenciesInProject()));
-                sb.append("\n");
-                sb.append("Missing plugins in lockfile:\n ");
-                sb.append(JsonUtils.toJson(diff.getMissingPluginsInFile()));
-                sb.append("\n");
-                sb.append("Missing plugins in project:\n ");
-                sb.append(JsonUtils.toJson(diff.getMissingPluginsInProject()));
-                sb.append("\n");
-                throw new MojoExecutionException("Failed verifying lock file" + sb);
+                        + lockFileFromProject.getVersion().getValue() + "\n" + "Missing dependencies in lock file:\n "
+                        + JsonUtils.toJson(diff.getMissingDependenciesInFile())
+                        + "\n"
+                        + "Missing dependencies in project:\n "
+                        + JsonUtils.toJson(diff.getMissingDependenciesInProject())
+                        + "\n"
+                        + "Missing plugins in lockfile:\n "
+                        + JsonUtils.toJson(diff.getMissingPluginsInFile())
+                        + "\n"
+                        + "Missing plugins in project:\n "
+                        + JsonUtils.toJson(diff.getMissingPluginsInProject())
+                        + "\n";
+                throw new MojoExecutionException("Failed verifying lock file" + sa);
             }
         } catch (IOException e) {
             throw new MojoExecutionException("Could not read lock file", e);
--- maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/reporting/LockFileDifference.java
+++ maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/reporting/LockFileDifference.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Sets;
 import io.github.chains_project.maven_lockfile.data.LockFile;
 import io.github.chains_project.maven_lockfile.data.MavenPlugin;
 import io.github.chains_project.maven_lockfile.graph.DependencyNode;
+import org.apache.maven.artifact.Artifact;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,34 +13,87 @@ public class LockFileDifference {
 
     private final Set<DependencyNode> missingDependenciesInProject;
     private final Set<DependencyNode> missingDependenciesInFile;
+    private final Set<DependencyNode> dependenciesWithInvalidChecksums;
 
     private final Set<MavenPlugin> missingPluginsInProject;
     private final Set<MavenPlugin> missingPluginsInFile;
+    private final Set<MavenPlugin> pluginsWithInvalidChecksums;
 
     private LockFileDifference(
             Set<DependencyNode> missingDependenciesInProject,
             Set<DependencyNode> missingDependenciesInFile,
+            Set<DependencyNode> dependenciesWithInvalidChecksums,
             Set<MavenPlugin> missingPluginsInProject,
-            Set<MavenPlugin> missingPluginsInFile) {
+            Set<MavenPlugin> missingPluginsInFile,
+            Set<MavenPlugin> pluginsWithInvalidChecksums) {
         this.missingDependenciesInProject = missingDependenciesInProject;
         this.missingDependenciesInFile = missingDependenciesInFile;
+        this.dependenciesWithInvalidChecksums = dependenciesWithInvalidChecksums;
         this.missingPluginsInProject = missingPluginsInProject;
         this.missingPluginsInFile = missingPluginsInFile;
+        this.pluginsWithInvalidChecksums = pluginsWithInvalidChecksums;
     }
 
+    // Change this to check for checksum and handle it differently
     public static LockFileDifference diff(LockFile lockFileFromFile, LockFile lockFileFromProject) {
         Set<DependencyNode> dependenciesFromFile = new HashSet<>(lockFileFromFile.getDependencies());
         Set<DependencyNode> dependenciesFromProject = new HashSet<>(lockFileFromProject.getDependencies());
         Set<DependencyNode> missingDependenciesInProject =
                 Sets.difference(dependenciesFromFile, dependenciesFromProject);
         Set<DependencyNode> missingDependenciesInFile = Sets.difference(dependenciesFromProject, dependenciesFromFile);
+        Set<DependencyNode> dependenciesWithInvalidChecksums = getTamperedDependencies(dependenciesFromFile,
+                dependenciesFromProject);
         Set<MavenPlugin> pluginsFromFile = new HashSet<>(lockFileFromFile.getMavenPlugins());
         Set<MavenPlugin> pluginsFromProject = new HashSet<>(lockFileFromProject.getMavenPlugins());
         Set<MavenPlugin> missingPluginsInProject = Sets.difference(pluginsFromFile, pluginsFromProject);
         Set<MavenPlugin> missingPluginsInFile = Sets.difference(pluginsFromProject, pluginsFromFile);
+        Set<MavenPlugin> pluginsWithInvalidChecksums = getTamperedPlugins(pluginsFromFile, pluginsFromProject);
 
         return new LockFileDifference(
-                missingDependenciesInProject, missingDependenciesInFile, missingPluginsInProject, missingPluginsInFile);
+                missingDependenciesInProject, missingDependenciesInFile, dependenciesWithInvalidChecksums,
+                missingPluginsInProject, missingPluginsInFile, pluginsWithInvalidChecksums);
+    }
+
+    private static HashSet<DependencyNode> getTamperedDependencies(Set<DependencyNode> dependenciesFromFile,
+                                                               Set<DependencyNode> dependenciesFromProject) {
+        HashSet<DependencyNode> tamperedDependencies = new HashSet<>();
+        for (DependencyNode dependencyFromFile : dependenciesFromFile) {
+            for (DependencyNode dependencyFromProject : dependenciesFromProject) {
+                if (isDependencyTamperedWith(dependencyFromFile, dependencyFromProject)) {
+                    tamperedDependencies.add(dependencyFromFile);
+                }
+            }
+        }
+        return tamperedDependencies;
+    }
+
+    private static boolean isDependencyTamperedWith(DependencyNode dependencyFromFile, DependencyNode dependencyFromProject) {
+        return dependencyFromFile.getArtifactId().equals(dependencyFromProject.getArtifactId()) &&
+                dependencyFromFile.getGroupId().equals(dependencyFromProject.getGroupId()) &&
+                dependencyFromFile.getVersion().equals(dependencyFromProject.getVersion()) &&
+                dependencyFromFile.getChecksumAlgorithm().equals(dependencyFromProject.getChecksum()) &&
+                !dependencyFromFile.getChecksum().equals(dependencyFromProject.getChecksum());
+    }
+
+    private static HashSet<MavenPlugin> getTamperedPlugins(Set<MavenPlugin> pluginsFromFile,
+                                                           Set<MavenPlugin> pluginsFromProject) {
+        HashSet<MavenPlugin> tamperedPlugins = new HashSet<>();
+        for (MavenPlugin pluginFromFile : pluginsFromFile) {
+            for (MavenPlugin pluginFromProject : pluginsFromProject) {
+                if (isPluginTamperedWith(pluginFromFile, pluginFromProject)) {
+                   tamperedPlugins.add(pluginFromFile);
+                }
+            }
+        }
+        return tamperedPlugins;
+    }
+
+    private static boolean isPluginTamperedWith(MavenPlugin pluginFromFile, MavenPlugin pluginFromProject) {
+        return pluginFromFile.getArtifactId().equals(pluginFromProject.getArtifactId()) &&
+                pluginFromFile.getGroupId().equals(pluginFromProject.getGroupId()) &&
+                pluginFromFile.getVersion().equals(pluginFromProject.getVersion()) &&
+                pluginFromFile.getChecksumAlgorithm().equals(pluginFromProject.getChecksum()) &&
+                !pluginFromFile.getChecksum().equals(pluginFromProject.getChecksum());
     }
 
     /**
@@ -47,12 +102,17 @@ public class LockFileDifference {
     public Set<DependencyNode> getMissingDependenciesInFile() {
         return new HashSet<>(missingDependenciesInFile);
     }
-
     /**
      * @return the missingDependenciesInProject
      */
     public Set<DependencyNode> getMissingDependenciesInProject() {
         return new HashSet<>(missingDependenciesInProject);
+    }
+    /**
+     * @return the dependenciesWithInvalidChecksums
+     */
+    public Set<DependencyNode> getDependenciesWithInvalidChecksums() {
+        return new HashSet<>(dependenciesWithInvalidChecksums);
     }
     /**
      * @return the missingPluginsInFile
@@ -65,5 +125,11 @@ public class LockFileDifference {
      */
     public Set<MavenPlugin> getMissingPluginsInProject() {
         return new HashSet<>(missingPluginsInProject);
+    }
+    /**
+     * @return the pluginsWithInvalidChecksums
+     */
+    public Set<MavenPlugin> getPluginsWithInvalidChecksums() {
+        return new HashSet<>(pluginsWithInvalidChecksums);
     }
 }
